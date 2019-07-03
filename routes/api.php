15f26b0775cc981ebd399b43400a18dce7594a7b
sergio <sergiotupaz@gmail.com>
2019-07-03 15:38:05 +0800

    CRUD for Question along with QuestionResource Transformation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::apiResource('/question','QuestionController');
